5318e1e5125c07d8dade755775ab2fb098279006
SaltyAimbOtter <saltyaimbotter@gmail.com>
2022-01-02 14:30:13 +0100

    refactor rmi integration

--- client/src/main/java/stk/students/Client.java
+++ client/src/main/java/stk/students/Client.java
@@ -1,22 +1,20 @@
 package stk.students;
 
 import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.util.ArrayList;
-import java.rmi.Naming;
 
 /**
  * Hello world!
  */
 public class Client {
- public static String ipAdress="127.0.0.1";
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-        QuestService server = (QuestService) Naming.lookup("rmi://"+ ipAdress +":1099/Quest_Server");
-        //Registry registry = LocateRegistry.getRegistry(1099);
 
+    public static String ipAddress = "127.0.0.1";
+
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        QuestService server = (QuestService) Naming.lookup("rmi://" + ipAddress + ":1099/Quest_Server");
 
         String responseMessage = server.sendMessage("Hallo!");
         System.out.println("[Server]: " + responseMessage);
--- server/src/main/java/stk/students/Server.java
+++ server/src/main/java/stk/students/Server.java
@@ -1,26 +1,27 @@
 package stk.students;
 
 import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.rmi.Naming;
 
 /**
  * Hello world!
  */
 public class Server {
-    public static String ipAdress ="127.0.0.1";
+
+    public static String ipAddress = "127.0.0.1";
+
     public static void main(String[] args) throws RemoteException, AlreadyBoundException, java.net.MalformedURLException {
-        final QuestService questService = new QuestServiceImpl();
-        QuestService stub = (QuestService) UnicastRemoteObject.exportObject(questService, 1099);
+        QuestService stub = (QuestService) UnicastRemoteObject.exportObject(new QuestServiceImpl(), 1099);
 
         Registry registry = LocateRegistry.createRegistry(1099);
         registry.bind("QuestService", stub);
 
-        Naming.bind("rmi://"+ ipAdress +":1099/Quest_Server", questService);
+        Naming.bind("rmi://" + ipAddress + ":1099/Quest_Server", stub);
 
-        }
     }
+}
 
