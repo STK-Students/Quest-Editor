7bdb7a658cf83751f42b3a75175b1e74362be874
ikarus <ikarus.it.s@gmail.com>
2022-01-04 20:09:46 +0100

    update QuestService

--- api/src/main/java/stk/students/QuestService.java
+++ api/src/main/java/stk/students/QuestService.java
@@ -8,4 +8,11 @@ import java.util.List;
 public interface QuestService extends Remote {
     boolean loginUser(String email, String password) throws RemoteException;
     boolean registerUser(String email, String username, String password) throws RemoteException;
+    void createDefaultRole() throws RemoteException;
+    void disconnectUser(User user) throws RemoteException;
+    boolean createRole(String name, Color color);
+    boolean userAlreadyExists(User user);
+    boolean roleAlreadyExists(Role role);
+    void assignUserToRole(User user, Role role);
+    boolean userHasRole(User user, Role role);
 }
