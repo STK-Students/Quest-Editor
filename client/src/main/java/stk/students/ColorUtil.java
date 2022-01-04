b36b13183e8129e1ae11c1bb3108a3a98227d9e0
SaltyAimbOtter <saltyaimbotter@gmail.com>
2022-01-04 14:07:20 +0100

    refactor Colors, ConfigManager.java, WindowAPI

--- client/src/main/java/stk/students/ColorUtil.java
+++ client/src/main/java/stk/students/ColorUtil.java
@@ -1,5 +1,8 @@
 package stk.students;
 
+
+import static stk.students.Color.RESET;
+
 public class ColorUtil {
 
     public static String colorize(String input, Color... colors) {
@@ -7,51 +10,6 @@ public class ColorUtil {
         for (Color color : colors) {
             result.append(color.getColorCode());
         }
-        return result + input + Color.RESET.getColorCode();
-    }
-
-    public enum Color {
-        //This
-        RESET("0"),
-
-        //Normal Colours
-        RED("31"),
-        GREEN("32"),
-        YELLOW("33"),
-        BLUE("34"),
-        PURPLE("35"),
-        CYAN("36"),
-        WHITE("97"),
-
-        //Light Colours
-        LIGHT_RED("91"),
-        LIGHT_GREEN("92"),
-        LIGHT_YELLOW("93"),
-        LIGHT_BLUE("94"),
-        LIGHT_PURPLE("95"),
-        LIGHT_CYAN("96"),
-
-        //Bold
-        BOLD("1"),
-
-        //Underline and no Underline
-        UNDERLINE("4"),
-        NO_UNDERLINE("24"),
-
-        //Crazy shit
-        BLINK("5"),
-
-        //Default
-        DEFAULT("0");
-
-        private final String colorCode;
-
-        Color(String colorCode) {
-            this.colorCode = colorCode;
-        }
-
-        public String getColorCode() {
-            return "\033[" + colorCode + "m";
-        }
+        return result + input + RESET.getColorCode();
     }
 }
--- client/src/main/java/stk/students/ConfigManager.java
+++ client/src/main/java/stk/students/ConfigManager.java
@@ -3,6 +3,7 @@ package stk.students;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Map;
 
 public class ConfigManager {
@@ -12,12 +13,48 @@ public class ConfigManager {
     /**
      * Contains all message strings from the message resource file.
      */
-    public static Map<String, Object> messages;
+    private Map<String, Object> configData;
 
     public ConfigManager() {
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(RESOURCE_FILE_NAME);
 
         Yaml yaml = new Yaml();
-        messages = yaml.load(inputStream);
+        configData = yaml.load(inputStream);
+    }
+
+    /**
+     * Gets the config as a parsed map.
+     * @return unmodifiable view of the config
+     */
+    public Map<String, Object> getConfigData() {
+        return Collections.unmodifiableMap(configData);
+    }
+
+    /**
+     * Gets a string message from the config.
+     * You can only use this on method on config keys that hold a string.
+     * @param configKey the config key
+     * @param colors colors to apply to the string
+     * @return a colorized config value
+     */
+    public String getMessage(String configKey, Color... colors) {
+        String message;
+        try {
+             message = (String) configData.get(configKey);
+        } catch (ClassCastException exception) {
+            throw new UnsupportedOperationException("You can only use this on method on config keys that hold a string.");
+
+        }
+        return ColorUtil.colorize(message, colors);
+    }
+
+    /**
+     * Gets a string message from the config.
+     * You can only use this on method on config keys that hold a string.
+     * @param configKey the config key
+     * @return a white config value
+     */
+    public String getMessage(String configKey) {
+        return getMessage(configKey, Color.WHITE);
     }
 }
